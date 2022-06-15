c70ec4949c812ce1624b7f2ee4d07362c8fa1e1e
Roy Christo <roy.biaga@nanodev.ca>
2022-06-15 22:11:18 +0100

    chore(app): mock react plotly chart

--- src/pages/index.test.jsx
+++ src/pages/index.test.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import Index from './index'
 
+jest.mock('react-plotly.js', () => () => <div>MockedChart</div>)
+
 describe('<Header />', () => {
 	it('renders snapshot', () => {
 		const tree = renderer.create(<Index />).toJSON()
